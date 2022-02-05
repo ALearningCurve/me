d83e1877d0c960ecd39c9404681d022baf90e82a
Will WS <willwallsoto@gmail.com>
2022-02-04 22:38:08 -0500

    fix toggle bug

--- scripts/index.js
+++ scripts/index.js
@@ -9,27 +9,34 @@ setupScrollAnimations()
 
 // Add collapsing divs
 const toggleDiv = (id) => {
-    const div = document.getElementById(id);
-    div.classList.toggle("show")
-    console.log("shown")
+
 
 
     // Scroll Snap Behavior does not play nice with some of the collapsing divs (ie. makes the page jump around)
     // So here we want to get the elements that have scroll snap and disable them temporarily
-    const snapping = document.getElementsByClassName('snap-y');
-    for (elem in snapping) {
-        elem.classList.toggle("snap-y")
+    const snapping = document.getElementsByClassName('snap');
+    console.log(snapping)
+    for (let i = 0; i < snapping.length; i++) {
+        const elem = snapping[i];
+        elem.classList.remove("snap-y");
     }
+
+    const div = document.getElementById(id);
+    div.classList.toggle("show")
+
     // reenable snapping
     setTimeout(() => {
-        for (elem in snapping) {
-            elem.classList.toggle("snap-y")
+        for (let i = 0; i < snapping.length; i++) {
+            console.log("hidden")
+            const elem = snapping[i];
+            elem.classList.add("snap-y");
         }
-    }, 2500)
+    }, 2000)
 }
 
 // Add listener for a collapsing div
 const resumeButton = document.getElementById("show-resume-button");
+
 resumeButton.onclick = () => {
     toggleDiv('resume-container')
 }
