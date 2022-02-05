c9d9f2434c796ccfe54010e08cfbdc0161a2d33a
Will WS <willwallsoto@gmail.com>
2022-02-04 20:39:43 -0500

    add resume

--- scripts/index.js
+++ scripts/index.js
@@ -5,3 +5,16 @@ import { startTypeWriter } from './typeWriter.js'
 insertInformation()
 startTypeWriter()
 setupScrollAnimations()
+
+
+const toggleDiv = (id) => {
+    const div = document.getElementById(id);
+    div.classList.toggle("show")
+    console.log("shown")
+}
+
+
+const resumeButton = document.getElementById("show-resume-button");
+resumeButton.onclick = () => {
+    toggleDiv('resume-container')
+}
